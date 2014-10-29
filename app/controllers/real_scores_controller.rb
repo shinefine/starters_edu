286ac535a8b4412daf87ad4216aede3787ed3d3f
shinefine <shinefine@gmail.com>
2014-10-29 11:14:20 +0800

    修正bug,学员真实考试成绩统计功能不正常显示

--- app/controllers/real_scores_controller.rb
+++ app/controllers/real_scores_controller.rb
@@ -4,7 +4,8 @@ class RealScoresController < ApplicationController
     #学员特定,列出学员的所有真实成绩
     @student =Student.find(params[:student_id])
 
-    @exam_type =params[:exam_type]
+    @training_class=TrainingClass.find(params[:training_class_id])
+    @exam_type =@training_class.exam_type
 
     if @exam_type=="SAT"
       @scores=@student.real_scores.sat.true_real
